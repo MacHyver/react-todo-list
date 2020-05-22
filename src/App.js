21b9be04222ead82473f63ade10c9d9c52174e36
MacHyver <mrs_machyver@hotmail.com>
2020-05-22 14:29:15 +0200

    Add the first lines

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx, css, Global, ClassNames } from '@emotion/core';
+
+const headerStyle = css`
+  background-color: lightgrey;
+  text-align: center;
+`;
+
+const mainStyle = css`
+  background-color: magenta;
+  text-align: center;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* <div css={css``}> */}
+      <header className="App-header" css={headerStyle}>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Todos this week</h1>
+        <input type="text"></input>
+        <input type="submit" value="+"></input>
       </header>
+      {/* </div> */}
+      <main css={mainStyle}>
+        <input type="Radio"></input>
+      </main>
     </div>
   );
 }
